457f5cb9304e1033bfe87d2dc62df70628c1c600
mainProyect <miguelsalas19941@gmail.com>
2024-10-21 16:29:35 -0300

    Update practicas.py

--- practicas.py
+++ practicas.py
@@ -1,3 +1,5 @@
+"""Se crea una clase PersonalePrincipal, la
+cual tiene distintos metodos de interaccion."""
 class PersonajePrincipal:
     def  __init__(self, nombre):
         if not isinstance(nombre,str):
@@ -7,9 +9,23 @@ class PersonajePrincipal:
         self.defensa = 1
         self.speed = 1
         self.attSpeed = 1
-    def  __str__():
+        self.nivel=1
+    def subirNivel(self):
+        self.nivel+1
+        return self.nivel
+    def cambiarDeClase(self):
+        if self.nivel=5:
+            print(f'{self.nombre}, ah alcanzado el nivel 5, cambia de clase.')
+            return Mago(self, nombre)
+        return self
+    def  __str__(self):
         return f'{self.nombre}\n    Ataque -> {self.ataque}\n   Defensa -> {self.defensa}\n    Speed -> {self.speed}\n    Velocidad de ataque -> {self.attSpeed}'
+    
 
+
+class Mago(PersonajePrincipal):
+    def __init__(self, nombre):
+        super().__init__(self,nombre)
 
 miProtagonista = PersonajePrincipal("Robot prueba 'A'")
 print(miProtagonista.__str__())        
