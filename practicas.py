540963e338db47002f00ce2fab973e8134a4b9a7
mainProyect <miguelsalas19941@gmail.com>
2024-10-22 12:19:11 -0300

    asdasd

--- practicas.py
+++ practicas.py
@@ -36,17 +36,6 @@ class Mago(PersonajePrincipal):
     def __init__(self, nombre):
         super().__init__(nombre)
 
-class Gobling:
-    def __init__(self,nombre):
-        self.nombre = nombre
-        self.vida = 100
-        self.mana = 100
-        self.defensa = 2
-        self.ataque = 3
-        self.speed = 3
-        self.attSpeed= 3
-    def __str__(self):
-        return f'{self.nombre}\n    Ataque -> {self.ataque}\n   Defensa -> {self.defensa}\n    Speed -> {self.speed}\n    Velocidad de ataque -> {self.attSpeed}\n    VIDA -> {self.vida}\n    MANA -> {self.mana}'
 
 
 
