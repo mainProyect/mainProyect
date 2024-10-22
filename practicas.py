45c08fdf37ed0e8d85d5f29562612d38a758f468
mainProyect <miguelsalas19941@gmail.com>
2024-10-22 10:11:48 -0300

    Agregar

--- practicas.py
+++ practicas.py
@@ -6,21 +6,21 @@ class PersonajePrincipal:
         if not isinstance(nombre,str):
             raise TypeError("El nombre tiene que ser una cadena de caranteres")
         self.nombre=nombre
-        self.ataque = 1
+        self.ataque = 10
         self.defensa = 1
         self.speed = 1
-        self.attSpeed = 1
+        self.attSpeed = 2
         self.nivel=1
     def subirNivel(self):
         self.nivel+1
         return self.nivel
-    def calcularDaño(self,enemigo,multiplicador=1.0)
+    def calcularDaño(self,enemigo,multiplicador=1.0):
         aleatorio=random.randint(-5,5)
         daño=(self.ataque*multiplicador)-enemigo.defensa+aleatorio
         return max(daño,0)
-    def atacar(self):
-        daño=calcularDaño(enemigo)
-        print({self.nombre})
+    def atacar(self, enemigo):
+        daño=self.calcularDaño(enemigo)
+        print(f'{self.nombre} le ah hecho {daño} de daño a {enemigo.nombre}')
     def cambiarDeClase(self):
         if self.nivel==5:
             print(f'{self.nombre}, ah alcanzado el nivel 5, cambia de clase.')
@@ -28,13 +28,27 @@ class PersonajePrincipal:
         return self
     def  __str__(self):
         return f'{self.nombre}\n    Ataque -> {self.ataque}\n   Defensa -> {self.defensa}\n    Speed -> {self.speed}\n    Velocidad de ataque -> {self.attSpeed}'
-    
-
-
 class Mago(PersonajePrincipal):
     def __init__(self, nombre):
         super().__init__(self,nombre)
 
+class Gobling:
+    def __init__(self,nombre):
+        self.nombre = nombre
+        self.vida = 100
+        self.mana = 100
+        self.defensa = 2
+        self.ataque = 3
+        self.speed = 3
+        self.attSpeed= 3
+    def __str__(self):
+        return f'{self.nombre}\n    Ataque -> {self.ataque}\n   Defensa -> {self.defensa}\n    Speed -> {self.speed}\n    Velocidad de ataque -> {self.attSpeed}\n    VIDA -> {self.vida}\n    MANA -> {self.mana}'
+
+
+
 miProtagonista = PersonajePrincipal("Robot prueba 'A'")
 print(miProtagonista.__str__())        
+miGoblin=Gobling("AllStar")
+
+miProtagonista.atacar(miGoblin)
 
