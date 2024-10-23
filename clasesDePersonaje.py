275c791dfa78e0ac319b4cf37eaeccf6da8edf1d
mainProyect <miguelsalas19941@gmail.com>
2024-10-23 14:25:20 -0300

    asd

---
+++ clasesDePersonaje.py
@@ -0,0 +1,3 @@
+class Mago(PersonajePrincipal):
+    def __init__(self, nombre):
+        super().__init__(nombre)
--- practicas.py
+++ practicas.py
@@ -11,20 +11,20 @@ class PersonajePrincipal:
         self.speed = 1
         self.attSpeed = 2
         self.nivel=1
-"""Metodo para subir de nivel a personaje mientas este Sea menor a 100"""
+    """Metodo para subir de nivel a personaje mientas este Sea menor a 100"""
     def subirNivel(self):
         self.nivel+1
         return self.nivel
-"""Metodo para calcular el daño de un personaje hacia otro"""
+    """Metodo para calcular el daño de un personaje hacia otro"""
     def calcularDaño(self,enemigo,multiplicador=1.0):
         aleatorio=random.randint(-5,5)
         daño=(self.ataque*multiplicador)-enemigo.defensa+aleatorio
         return max(daño,0)
-"""Metodo en el que se llama al que calcula el daño e imprime el daño hecho por el personaje a otro"""
+    """Metodo en el que se llama al que calcula el daño e imprime el daño hecho por el personaje a otro"""
     def atacar(self, enemigo):
         daño=self.calcularDaño(enemigo)
         print(f'{self.nombre} le ah hecho {daño} de daño a {enemigo.nombre}')
-"""Segun el nivel del personaje va a tener distintas opciones para cambiar de clase"""
+    """Segun el nivel del personaje va a tener distintas opciones para cambiar de clase"""
     def cambiarDeClase(self):
         if self.nivel==5:
             print(f'{self.nombre}, ah alcanzado el nivel 5, cambia de clase.')
@@ -32,9 +32,6 @@ class PersonajePrincipal:
         return self
     def  __str__(self):
         return f'{self.nombre}\n    Ataque -> {self.ataque}\n   Defensa -> {self.defensa}\n    Speed -> {self.speed}\n    Velocidad de ataque -> {self.attSpeed}'
-class Mago(PersonajePrincipal):
-    def __init__(self, nombre):
-        super().__init__(nombre)
 
 
 
