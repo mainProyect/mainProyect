f2633e99b15c40c7ccb727203bb44993e110359c
mainProyect <miguelsalas19941@gmail.com>
2024-10-25 19:02:27 -0300

    Update practicas.py
    Iniciando practicas de el T.P
    Se actualiza la clase con atributos nuevos tipo Lineage2

--- practicas.py
+++ practicas.py
@@ -6,15 +6,31 @@ class PersonajePrincipal:
         if not isinstance(nombre,str):
             raise TypeError("El nombre tiene que ser una cadena de caranteres")
         self.nombre=nombre
-        self.ataque = 10
-        self.defensa = 1
+        self.pAttack= 10
+        self.mAttack = 1
+        self.mDef=1
+        self.pDef=1
+        self.accuracy=1
+        self.evacion=1
+        self.rateCritical= 1
+        self.castingSpeed= 1
         self.speed = 1
         self.attSpeed = 2
         self.nivel=1
     """Metodo para subir de nivel a personaje mientas este Sea menor a 100"""
-    def subirNivel(self, incrementoDeAtaque=1, incrementoDefensa = 1, incrementoAttspeed=1):
+    def subirNivel(self, incrementoPAttack=1, 
+                   inceementomMAttack=1
+                   incrementoPDef = 1,
+                   incrementoMDef=1,
+                   incrementoAccuracy=1,
+                   incrementoEvacion=1,
+                   incrementoRateCritical=1,
+                   incrementoCastingSpeed=1,
+                   incrementoSpeed=1,
+                   incrementoAttspeed=1):
+            if not isinstance(self,PersonajePrincipal())
         self.nivel+=1
-        self.ataque+=incrementoDeAtaque
+        self.pAttack+=incrementoPAttack
         self.incrementoDefensa+=incrementoDefensa
         self.incrementoAttspeed+=incrementoAttspeed
 
@@ -36,7 +52,11 @@ class PersonajePrincipal:
         return self
     def  __str__(self):
         return f'{self.nombre}\n    Ataque -> {self.ataque}\n   Defensa -> {self.defensa}\n    Speed -> {self.speed}\n    Velocidad de ataque -> {self.attSpeed}'
-
+class Mago(PersonajePrincipal):
+      def __init__(self):
+          super.__init__(nombre):
+          self.ataque
+            
 
 
 
