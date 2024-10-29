6a33629370dfc2b86a9f55ebc227a8b77ee2eeb5
mainProyect <miguelsalas19941@gmail.com>
2024-10-29 08:01:48 -0300

    Update clasesDePersonajes.py
    Se agrego clase Mago en clase de personajes.py

---
+++ clasesDePersonajes.py
@@ -0,0 +1,25 @@
+class Mago(PersonajePrincipal):
+      def __init__(self,incrementoPAttack=1, 
+                   incrementoMAttack=1,
+                   incrementoMDef=5,
+                   incrementoPDef = 2,
+                   incrementoAccuracy=1,
+                   incrementoEvacion=2,
+                   incrementoRateCritical=1,
+                   incrementoCastingSpeed=5,
+                   incrementoSpeed=1,
+                   incrementoAttspeed=1):
+          super.__init__(nombre):
+            self.nivel+=1
+            self.vida = 100
+            self.mana = 100
+            self.pAttack= incrementoPAttack
+            self.mAttack = incrementoMAttack
+            self.mDef=incrementoMDef
+            self.pDef=incrementoPDef
+            self.accuracy=incrementoAccuracy
+            self.evacion=incrementoEvacion
+            self.rateCritical= incrementoRateCritical
+            self.castingSpeed= incrementoCastingSpeed
+            self.speed = incrementoSpeed
+            self.attSpeed = incrementoAttspeed
